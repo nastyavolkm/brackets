bf26976ecd4ffabf1083cf2f19f4e43504fba83c
nastya <nastyavolkm@tut.by>
2021-03-10 19:42:21 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split("");
+      for(let i =0; i < str.length; i++){
+      
+      for(let j=0; j < bracketsConfig.length; j++){
+        
+      if(str[i] === bracketsConfig[j][0] && str[i+1] === bracketsConfig[j][1]){
+          str.splice(i, 2);
+          i = -1;
+        }
+      }
+    }
+ 
+    if(str.length == 0){
+      return true;
+    } else {
+      return false;
+    }
+  
 }
